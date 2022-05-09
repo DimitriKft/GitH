b0119c59bdb704f196d1582c2dd43e1390b81049
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-09 12:34:35 +0200

    Initial Commit

---
+++ TokTik/TokTikApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokTikApp.swift
+//  TokTik
+//
+//  Created by dimitri on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TokTikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
